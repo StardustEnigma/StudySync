ccae364fb3c24be07346678a60bd240b90e41a29
Atharva <atharvamandle19@gmail.com>
2025-05-27 23:14:10 +0530

    day1

--- web/web/urls.py
+++ web/web/urls.py
@@ -21,5 +21,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home ,name='home'),
+    path('about',views.about,name='about'),
+    path('blog',views.blog,name='blog'),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
--- web/web/views.py
+++ web/web/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 
 def home(request):
-    return render(request,'home.html')
+    return render(request,'website/home.html')
+
+def about(request):
+    return render(request,'website/about.html')
+
+def blog(request):
+    return render(request,'website/blog.html')
